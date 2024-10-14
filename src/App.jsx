b2552eda4733e8ad62a569142bf41fc340c4f928
tejasree <tejasreereddy1899@gmail.com>
2024-10-14 19:39:33 +0530

    inmit2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <div>
      <h2>hello</h2>
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero porro, vel culpa ex omnis sequi.
+     </p>
     </div>
   )
 }
